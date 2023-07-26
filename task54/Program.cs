a6075b786945400f2d63f68950470a9e642bf24a
AleksandrBK <lebedevavlv@gmail.com>
2023-07-26 20:53:26 +0400

    Task 54

--- task54/Program.cs
+++ task54/Program.cs
@@ -10,4 +10,81 @@
 // 9 5 3 2
 // 8 4 4 2
 
+Task54();
+void Task54()
+{
+    Console.WriteLine("Введите количество строк: ");
+    int rows = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("Введите количество cтолбцов: ");
+    int columns = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("Введите минимальное значение элемента: ");
+    int min = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("Введите максимальное значение элемента: ");
+    int max = Convert.ToInt32(Console.ReadLine());
+
+    int[,] result = GetMatrix(rows, columns, min, max);
+
+    PrintMatrix(result, 0);
+    Console.WriteLine("Отсортированный массив чисел:");
+    SortMatrix(result, 0);
+
+
+    int[,] GetMatrix(int m, int n, int min, int max)
+    {
+        int[,] matrix = new int[m, n];
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                matrix[i, j] = new Random().Next(min, max + 1);
+            }
+        }
+        return matrix;
+    }
+
+    void PrintMatrix(int[,] matrix, int decimalPlaces)
+    {
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                string formattedNumber = matrix[i, j].ToString($"F{decimalPlaces}");
+                Console.Write(formattedNumber.PadRight(5));
+            }
+            Console.WriteLine();
+        }
+    }
+
+
+    void SortMatrix(int[,] matrix, int decimalPlaces)
+    {
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(1) - 1; j++)
+            {
+                for (int k = 0; k < matrix.GetLength(1) - j - 1; k++)
+                {
+                    if (matrix[i, k] < matrix[i, k + 1])
+                    {
+                        int temp = matrix[i, k];
+                        matrix[i, k] = matrix[i, k + 1];
+                        matrix[i, k + 1] = temp;
+                    }
+                }
+            }
+        }
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                string formattedNumber = matrix[i, j].ToString($"F{decimalPlaces}");
+                Console.Write(formattedNumber.PadRight(5));
+            }
+            Console.WriteLine();
+        }
+    }
+}
 
